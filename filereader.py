3e168e5ec5c1f0cd23b857d1ce89758c8fe8af6d
Robin <robinds@student.matnat.uio.no>
2014-06-01 17:37:31 +0200

    Improved file reader with small formatting fixes.

--- filereader.py
+++ filereader.py
@@ -11,18 +11,20 @@ def main():
     symbolLookup = dict(zip(tables.W_SYMBOLS, tables.U_SYMBOLS))
     f = open(argv[1], 'r')
     for line in f:
+        if line is '\n':
+            continue
+        stdout.write("\n%s" % (line))
+        # print()
         words = line.split()
         for d in words:
             d = d.strip()
             if d in tables.W_SYMBOLS:
                 stdout.write(symbolLookup[d])
-                # print(symbolLookup[d])
                 continue
             s.wylie = d
             t.analyze(s)
             s.tsheg()
             stdout.write(s.uni)
-        print()
     print()
     f.close()
 
